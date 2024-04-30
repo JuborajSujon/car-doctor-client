302be3d1483f1dae7c73f3160f66fa751cdc33d4
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-01 02:08:29 +0600

    Added Home section component

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/main";
+import Home from "../pages/Home/Home/Home";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <div>About</div>,
+      },
+      {
+        path: "/contact",
+        element: <div>Contact</div>,
+      },
+    ],
   },
 ]);
 
