ee64c73ac797b0ab251596a7ae723561297526c7
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-01 12:27:10 +0600

    defined header and footer component and assets file

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Main from "../Layout/main";
+import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 
 const router = createBrowserRouter([
